1d3bbd5c7347783ae987a0bca7acab387f05dbb0
chaos0103 <lyt1228@naver.com>
2022-01-23 23:27:56 +0900

    구현

---
+++ Part03 알고리즘 유형별 기출문제/Chapter12 구현 문제/Q09 문자열 압축.py
@@ -0,0 +1,15 @@
+def solution(s):
+    pass
+
+
+test_case1 = 'aabbaccc'
+test_case2 = 'ababcdcdababcdcd'
+test_case3 = 'abcabcdede'
+test_case4 = 'abcabcabcabcdededededede'
+test_case5 = 'xababcdcdababcdcd'
+
+print(solution(test_case1))  # 7
+print(solution(test_case2))  # 9
+print(solution(test_case3))  # 8
+print(solution(test_case4))  # 14
+print(solution(test_case5))  # 17
