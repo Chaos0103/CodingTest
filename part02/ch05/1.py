6aa24ac637313706b24575345112d0de9b1b9bcb
chaos0103 <lyt1228@naver.com>
2022-04-23 15:38:31 +0900

    update 22.03.10

--- part02/ch05/1.py
+++ part02/ch05/1.py
@@ -1,12 +1,15 @@
 n, m = map(int, input().split())
-data = [list(map(int, input())) for _ in range(n)]
+
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input())))
 
 
 def dfs(x, y):
     if x < 0 or y < 0 or x >= n or y >= m:
         return False
-    if data[x][y] == 0:
-        data[x][y] = 1
+    if graph[x][y] == 0:
+        graph[x][y] = 1
         dfs(x+1, y)
         dfs(x-1, y)
         dfs(x, y+1)
@@ -22,14 +25,3 @@ for i in range(n):
             result += 1
 
 print(result)
-
-'''
-[input]
-4 5
-00110
-00011
-11111
-00000
-[output]
-3
-'''
