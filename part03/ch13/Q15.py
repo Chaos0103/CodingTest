e815b6109378d78d74375352ffc4b1ae8c2e1a45
chaos0103 <lyt1228@naver.com>
2022-04-23 16:09:48 +0900

    update 22.04.23

--- part03/ch13/Q15.py
+++ part03/ch13/Q15.py
@@ -1,31 +1,33 @@
 from collections import deque
 
-n, m, k, start = map(int, input().split())
+n, m, target, start = map(int, input().split())
+
 graph = [[] for _ in range(n+1)]
+
+visited = [-1] * (n+1)
+
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
-visited = [-1] * (n+1)
 
+
+visited[start] = 0
 q = deque()
 q.append(start)
-visited[start] = 0
 while q:
-    now = q.popleft()
-    for i in graph[now]:
+    city = q.popleft()
+    for i in graph[city]:
         if visited[i] == -1:
-            visited[i] = visited[now] + 1
+            visited[i] = visited[city] + 1
             q.append(i)
 
 result = []
 for i in range(1, n+1):
-    if visited[i] == k:
+    if visited[i] == target:
         result.append(i)
 
 if len(result) == 0:
     print(-1)
 else:
-    result.sort()
-    for n in result:
-        print(n)
-
+    for num in result:
+        print(num)
